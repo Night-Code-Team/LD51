13629101dc1223ecf8faf432c1c52b23fa0bd5c4
Maxim Bodrov <activer12@mail.ru>
2022-10-03 22:56:59 +0300

    0.2.8
    Исправлено katuns и bleaks

--- Source Code/Scripts/Game/NPC/Beaks.cs
+++ Source Code/Scripts/Game/NPC/Beaks.cs
@@ -1,19 +1,19 @@
 public class Beaks : NPC
 {
-    public override int HP { get; set; }
-    public override int MaxHP { get; set; } = 100;
-    public override int Damage { get; protected set; } = 1;
-    public override float Speed { get; protected set; } = 5;
-    protected override void Move(Vector3 dest)
-    {
-        base.Move(dest);
-    }
-    protected override void Attack(Building target)
-    {
-        base.Attack(target);
-    }
-    public override void _Ready()
-    {
-        GetChild<AnimationPlayer>(1).Play("Rest");
-    }
+	public override int HP { get; set; }
+	public override int MaxHP { get; set; } = 100;
+	public override int Damage { get; protected set; } = 1;
+	public override float Speed { get; protected set; } = 5;
+	protected override void Move(Vector3 dest)
+	{
+		base.Move(dest);
+	}
+	protected override void Attack(Building target)
+	{
+		base.Attack(target);
+	}
+	public override void _Ready()
+	{
+		GetChild<AnimationPlayer>(1).Play("Rest");
+	}
 }
